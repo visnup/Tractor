3810c866018abb5d2bfb3092a11550aa69a9dd94
Visnu Pitiyanuvath <visnup@gmail.com>
2018-04-16 23:41:22 -0700

    Initial Commit

---
+++ Tractor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Tractor
+//
+//  Created by Visnu on 4/16/18.
+//  Copyright © 2018 Visnu Pitiyanuvath. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ Tractor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tractor
+//
+//  Created by Visnu on 4/16/18.
+//  Copyright © 2018 Visnu Pitiyanuvath. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
